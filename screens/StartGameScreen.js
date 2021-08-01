0a1c90e7d2e172bf09007fb90bd5b1bc75d8051a
Marcelo Júnior <marcelohjw@gmail.com>
2021-08-01 13:58:30 -0300

    More style on the main screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const StartGameScreen = props => {
     return(
         <View style={styles.screen}>
-            <Text>GAME SCREEN</Text>
+            <Text style={styles.title}>Começar!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Selecione um número</Text>
+                <TextInput />
+                <View style={styles.buttonsContainer}>
+                    <Button title="Recomeçar"/>
+                    <Button title="Confirmar"/>
+                </View>
+            </View>
         </View>
     );
 };
@@ -14,6 +22,30 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        padding: 15,
+        borderRadius: 6
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 });
 
