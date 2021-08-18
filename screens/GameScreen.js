32d4aa463565f4d22e7d780b5bdb8af7d5e8b1f0
Marcelo Júnior <marcelohjw@gmail.com>
2021-08-18 07:27:05 -0300

    If condition on dimensions style

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, Image, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Alert, Image, ScrollView, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 15 : 5,
         width: 300,
         maxWidth: '80%'
     },
